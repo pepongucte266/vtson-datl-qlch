e705f96146904e03cd155645c0942bc203d0921c
pepongcute266 <pepongcute266@gmail.com>
2022-12-11 16:55:39 +0700

    [FE] add axios

--- datn_qlch_fe/src/main.ts
+++ datn_qlch_fe/src/main.ts
@@ -7,6 +7,10 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 
+//Axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -27,6 +31,7 @@ const vuetify = createVuetify({
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(VueAxios, axios)
 app.use(router);
 app.use(vuetify);
 
